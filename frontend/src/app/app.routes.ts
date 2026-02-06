262887bca15316b9568e245fc6a8768306eb4dcd
Sofia Torres <mariasofiatm@gmail.com>
2026-02-06 12:15:48 -0500

    C1: Diseño y desarrollo de login responsive

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { Component } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        component: Login
+    },
+
+];
---
+++ frontend/src/app/components/login/login.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [CommonModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+
+}
